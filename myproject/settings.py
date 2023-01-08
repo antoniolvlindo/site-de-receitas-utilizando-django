bf8e437a7aab71efaf1c958ccb933e3af3c9788b
Antonio Lindo <antoniolvitall@gmail.com>
2023-01-08 14:35:37 -0300

    Criando DB

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'antonio_receita',
+        'USER': 'postgres',
+        'PASSWORD': '835247',
+        'HOST': 'localhost'
     }
 }
 
