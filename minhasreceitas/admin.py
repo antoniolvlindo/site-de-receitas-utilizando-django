7f2b8f418ce337afd57991dbc3e14eb44a48e52c
Antonio Lindo <antoniolvitall@gmail.com>
2023-01-08 17:11:15 -0300

    Chamando receita.objects

--- minhasreceitas/admin.py
+++ minhasreceitas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Receita
+
+admin.site.register(Receita)
+
+
 
 # Register your models here.
--- minhasreceitas/views.py
+++ minhasreceitas/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-
+from .models import Receita
 
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete'
-    }
+    receitas = Receita.objects.all()
+
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
     return render(request, 'index.html', dados)
 
